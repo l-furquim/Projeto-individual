9b5207774d969a9520bf9d9411f977abd3863197
l-furquim <lucas.furquim@sptech.school>
2024-10-10 12:18:23 -0300

    Início do backend do projeto

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2'); 
+
+const app = express();
+
+let db = mysql.createPool({
+      host: 'localhost',
+      user: 'usuario',
+      password: 'usuario@2003',
+      database: 'projeto-individual',
+      port: 3306
+}).promise();
+
+
+
+  app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+  });
+
+  app.listen(3300, () => {
+    console.log(`API iniciada na porta 3300`);
+  });
+
+  app.get("/", (_, response)=> {
+    return response.json("GET REALIZADO")
+  });
