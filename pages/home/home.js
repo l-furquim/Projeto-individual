843cd98b8b8cfe3838dab4d25d2836d8fba67f41
l-furquim <lucas.furquim@sptech.school>
2024-10-29 21:29:49 -0300

    Melhoria dos botões da home

--- pages/home/home.js
+++ pages/home/home.js
@@ -73,10 +73,11 @@ function fecharModal() {
 }
 
 function mostrarComentarios(botao, id){
-  
+
   const contribuicao = botao.closest('li');
   const containerComentario = contribuicao.querySelector('#containerComentario');
   console.log(contribuicao, containerComentario)
+  
   if(vezesClicadas == 1){
     vezesClicadas = 0;
     containerComentario.style.display = "none";
@@ -85,16 +86,15 @@ function mostrarComentarios(botao, id){
     <path fill-rule="evenodd" d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708z"/>
     </svg>`
   }else{
-  if(containerComentario){
-    vezesClicadas++;
-    containerComentario.style.display = "flex";
-    botaoMostrarComentario.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-90deg-down" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M4.854 14.854a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V3.5A2.5 2.5 0 0 1 6.5 1h8a.5.5 0 0 1 0 1h-8A1.5 1.5 0 0 0 5 3.5v9.793l3.146-3.147a.5.5 0 0 1 .708.708z"/>
-    </svg>`
+    if(containerComentario){
+      vezesClicadas++;
+      containerComentario.style.display = "flex";
+      botaoMostrarComentario.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-90deg-down" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M4.854 14.854a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V3.5A2.5 2.5 0 0 1 6.5 1h8a.5.5 0 0 1 0 1h-8A1.5 1.5 0 0 0 5 3.5v9.793l3.146-3.147a.5.5 0 0 1 .708.708z"/>
+      </svg>`
+    }
   }
-  }
-
 }
 
 function comentar(botao, id){
