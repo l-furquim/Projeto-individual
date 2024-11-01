b0ea69470146f3308f77785ddd399c6d2057e2a5
= <=>
2024-11-01 13:36:05 -0300

    Adição da funcionalidade de nova contribuição conectada ao banco

--- public/js/home.js
+++ public/js/home.js
@@ -137,3 +137,62 @@ function adicionarComentario(conteudo){
 function irContaUsuario(){
   location.replace("../dashboard/dashboard.html");
 }
+
+async function novaContribuicao(){
+  const conteudo = textAreaConteudoContribuicao.value;
+  const tipoContribuicao = selectTipo.value;
+  const filtro = categoriaFiltro.value;
+  const conteudoFiltro = opcoes.value;
+  console.log(conteudo);
+  var icone = "";
+
+  if(conteudo != undefined){
+    var mensagem = "";
+
+    botaoPostar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+          <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+          </svg>`;
+    
+
+    const resposta = await fetch("http://localhost:3333/contribuicao/cadastrar", {
+      method: "POST",
+      body: JSON.stringify({
+        conteudo: conteudo,
+        tipo: tipoContribuicao,
+        fkMaculado: 1,
+        tag: filtro,
+        conteudoTag: conteudoFiltro
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    if(resposta.ok){
+      mensagem = "Contribuição criada com sucesso!"
+      icone =  `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+      <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+      </svg>`;
+
+    }else{
+      resposta.json().then(m => {
+        mensagem = m.mensagem;
+        icone = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+              </svg>`;
+      });
+
+    }
+    setTimeout(()=> {
+      divMensagem.innerHTML = mensagem; // Nunca mais fazer isso...
+      botaoPostar.innerHTML = icone;
+      // erroSenha.style.border = estilo;
+
+      if(mensagem == "Contribuição criada com sucesso!"){
+        setTimeout(()=> {
+            location.reload();
+        }, 1000)
+      }
+
+    }, 2000);
+  }
+}
