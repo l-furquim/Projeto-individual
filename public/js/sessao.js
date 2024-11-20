e459a91e43697ebdd3db803e1d31da7c2bb0e5e5
= <=>
2024-11-20 09:43:05 -0300

    Adição da proteção das rotas pelo session storage e adição da verificacao se o usuario esta logado para ir ao inicio e voltar a home

---
+++ public/js/sessao.js
@@ -0,0 +1,37 @@
+function validarSessao(){
+  var idUsuario = sessionStorage.getItem("id");
+
+  
+
+
+}
+
+function validarRedirecionamento(idNav){
+  var idUsuario = sessionStorage.getItem("id");
+
+  if(idUsuario != null || idUsuario != undefined){
+    var nav = document.getElementById(idNav);
+
+    if(nav.id == "navInicio"){
+      nav.innerHTML = `
+      <a href="/inicio.html" class="selected">Início</a>
+      <a href="/home.html">Home</a>
+      <a id="ancoraDash">Dashboard</a>
+      `
+    }else if(nav.id == "navDashboard"){
+      nav.innerHTML = `
+      <a href="/inicio.html">Início</a>
+      <a href="/home.html">Home</a>
+      <a id="ancoraDash" class="selected">Dashboard</a>
+      `
+    }else{
+      nav.innerHTML = `
+                      <a href="/inicio.html">Início</a>
+                      <a href="/home.html" class="selected">Home</a>
+                      <a id="ancoraDash">Dashboard</a>
+                       `
+    }
+  }
+
+   
+}
